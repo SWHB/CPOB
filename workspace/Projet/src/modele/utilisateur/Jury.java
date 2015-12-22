ff9c15f206af2218b9eb2bc54ec6b3744e2e48ee
Schlange <will.schlange@gmail.com>
2015-12-22 18:51:52 +0100

    ajout Jury et fin controlUtilisateur

---
+++ workspace/Projet/src/modele/utilisateur/Jury.java
@@ -0,0 +1,68 @@
+package modele.utilisateur;
+
+import java.util.ArrayList;
+
+import modele.stage.Stage;
+
+public class Jury {
+	private ArrayList<Enseignant> lesEnseignant;
+	private Stage sonStage;
+	private Enseignant president;
+	
+	public Jury(Stage stg){
+		this.president=null;
+		this.sonStage = stg;
+		this.lesEnseignant = new ArrayList<Enseignant>();
+		this.lesEnseignant.add(this.sonStage.getEtudiantStage().getProfReferent());
+	}
+
+	public ArrayList<Enseignant> getLesEnseignant() {
+		return lesEnseignant;
+	}
+
+	public void setLesEnseignant(ArrayList<Enseignant> lesEnseignant) {
+		this.lesEnseignant = lesEnseignant;
+	}
+
+	public Stage getSonStage() {
+		return sonStage;
+	}
+
+	public void setSonStage(Stage sonStage) {
+		this.sonStage = sonStage;
+	}
+	
+	public void ajoutEnseignant(Enseignant ens){
+		this.lesEnseignant.add(ens);
+	}
+	
+	public Etudiant getEtudiant(){
+		return this.sonStage.getEtudiantStage();
+	}
+	
+	public Enseignant getPresident() {
+		return president;
+	}
+
+	public void setPresident(Enseignant president) {
+		this.president = president;
+	}
+	/**
+	 * Renvoi si le jury est prèt:
+	 *  0: pas prêt
+	 *  1: prêt
+	 *  2: manque le president
+	 * @return
+	 */
+	public int juryPret(){
+		if(lesEnseignant.size()>2){
+			if(this.president==null){
+				return 2;
+			}
+			return 1;
+		}
+		else{
+			return 0;
+		}
+	}
+}
